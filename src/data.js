8289dce66365ade59cf83f2f13a6facb2ff8c263
Humza-A <humzaaamir61@gmail.com>
2023-03-30 19:21:16 -0400

    Added: Question page

--- src/data.js
+++ src/data.js
@@ -2,8 +2,7 @@ export const headerData = {
   links: [
     {
       text: 'Home',
-      href: '/'
-      
+      href: '/',
     },
     {
       text: 'How It Works',
@@ -24,28 +23,87 @@ export const headerData = {
     },
   ],
 };
-  
+
+export const Category = {
+  Authority: "The credibility and expertise of the source of information",
+  Accuracy : "The truthfulness and correctness of the information presented",
+  Currency : "The timeliness and relevance of the information presented",
+  Coverage : "The scope and completeness of the information presented"
+}
+
+export const QuestionsData = {
+  questions: [
+    {
+      Question:
+        'If the topic is in a specialized field. Is it a reputable news organization or a reputable expert in the field? Does the writer have the academic background in the field to back up their claim?',
+      Category: 'Authority',
+      Why: 'Anyone can call themself an expert and write things they don’t fully understand to get clicks. It’s important we get our information from people actually who put in the time studying the topic and know what they are talking about.',
+      Ans: null,
+    },
+    {
+      Question: 'Q2',
+      Category: 'Authority',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: 'Q3',
+      Category: 'Authority',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '...',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+    {
+      Question: '',
+      Category: '',
+      Why: '',
+      Ans: null,
+    },
+  ],
+};
+
 export const footerData = {
   links: [
     {
       title: 'Home',
-      links: [
-        { text: 'Developer API', href: '#' },
-
-      ],
+      links: [{ text: 'Developer API', href: '#' }],
     },
     {
       title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-            ],
+      links: [{ text: 'Docs', href: '#' }],
     },
     {
       title: 'Company',
-      links: [
-        { text: 'About', href: 'blog' },
-      ],
+      links: [{ text: 'About', href: 'blog' }],
     },
   ],
-  
 };
