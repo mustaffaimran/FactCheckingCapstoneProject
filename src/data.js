9df6b4edbe9c3b7c1dca937a8b87154bc322a664
mustafa.imran123@gmail.com <mustafa.imran123@gmail.com>
2023-04-04 17:16:22 -0400

    changed questions and the whys

--- src/data.js
+++ src/data.js
@@ -27,50 +27,50 @@ export const QuestionsData = {
     {
       Question: 'Is the source reputable and trustworthy?',
       Category: 'Authority',
-      Why: 'This shows how many agree with this writer. If they are well-respected by many others that means this person is perceived as trustworthy.',
+      Why: 'This is important because using sources that are not credible or trustworthy can result in the dissemination of false information, which can have a negative impact on the reader.',
       Ans: undefined,
     },
     {
       Question:
         'Is the information supported by reliable evidence, such as statistics?',
       Category: 'Authority',
-      Why: 'Anyone can call themself an expert and write things they don’t fully understand to get clicks. It’s important we get our information from people actually who put in the time studying the topic and know what they are talking about.',
+      Why: 'This is important because it ensures that the information presented is factual and not based on personal opinions or beliefs.',
       Ans: undefined,
     },
     {
       Question: 'Are there multiple sources confirming the information?',
       Category: 'Accuracy',
-      Why: 'If multiple sources confirm the information, then it’s likely accurate. However, if there are conflicting reports, then It’s more questionable whether if what they say is true',
+      Why: 'This is important because it adds credibility to the information presented, as multiple sources confirming the same information make it more likely to be accurate.',
       Ans: undefined,
     },
     {
       Question: 'Does the writer have academic background or expertise in the field related to the topic?',
       Category: 'Accuracy',
-      Why: 'if there is no evidence to support the information, then the whole foundation of their argument is built on sand. Their hypothesis is likely not accurate.',
+      Why: 'This is important because it ensures that the writer has the knowledge and expertise necessary to accurately and comprehensively present the information.',
       Ans: undefined,
     },
     {
       Question: 'Is the information current and not outdated?',
       Category: 'Currency',
-      Why: 'Current events get updated frequently with new information, older information may quickly become outdated and irrelevant.',
+      Why: 'This is important because outdated information can be inaccurate, and therefore, not useful to the reader.',
       Ans: undefined,
     },
     {
       Question: 'Has the information been updated or revised recently?',
       Category: 'Currency',
-      Why: 'If the information has not been updated or revised recently, it may not be reliable or trustworthy. This question also helps to ensure that the information being presented is not based on outdated data or facts, which could lead to incorrect conclusions or actions.',
+      Why: 'This is important because information can change over time, and updates or revisions ensure that the information presented is accurate and up-to-date.',
       Ans: undefined,
     },
     {
       Question: 'Is the information presented in a comprehensive manner, covering all relevant aspects of the topic?',
       Category: 'Coverage',
-      Why: 'Conflicts of interest can affect the credibility and objectivity of the information being presented. If the source of the information has a financial or personal interest in promoting a particular viewpoint or agenda, they may be more likely to present biased or inaccurate information',
+      Why: 'This is important because comprehensive information ensures that the reader has a full understanding of the topic, and is not left with gaps or missing information.',
       Ans: undefined,
     },
     {
       Question: 'Are there any conflicts of interest that could bias the information presented?',
       Category: 'Coverage',
-      Why: 'If important details are missing, the information may be misleading or inaccurate, which can misinform readers and hinder their ability to make informed decisions. By ensuring that the information is comprehensive, readers can trust the source and have confidence in the accuracy of the information presented.',
+      Why: 'This is important because conflicts of interest can lead to biased information, which can be misleading to the reader. Disclosing any conflicts of interest helps readers make informed decisions about the credibility of the information presented.',
       Ans: undefined,
     },
   ],
@@ -88,9 +88,9 @@ export const footerData = {
     {
       title: 'How It Works?',
       links: [
-        { text: 'Guide', href: '#' },
-        { text: 'Examples', href: '#' },
-        { text: 'Additional Resources', href: '#' },
+        { text: 'Guide (WIP)', href: '#' },
+        { text: 'Examples(WIP)', href: '#' },
+        { text: 'Additional Resources(WIP)', href: '#' },
             ],
     },
     {
